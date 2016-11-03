acd4780d3241ff4fddbbe0717ad5fb4f1f2f9b0e
Noah Ziems <noah.ziems@live.com>
2016-11-03 07:47:43 -0400

    Implemented Convolutional Neural Networks

--- CS231n/assignment2/cs231n/classifiers/fc_net.py
+++ CS231n/assignment2/cs231n/classifiers/fc_net.py
@@ -232,9 +232,18 @@ class FullyConnectedNet(object):
          weight_scale * np.random.randn(dims[i], dims[i + 1]) for i in range(len(dims) - 1)}
     b = {'b' + str(i + 1): np.zeros(dims[i + 1])
          for i in range(len(dims) - 1)}
-
+    
     self.params.update(b)
     self.params.update(Ws)
+    
+    if self.use_batchnorm:
+        gammas = {'gamma' + str(i + 1):
+                  np.ones(dims[i + 1]) for i in range(len(dims) - 2)}
+        betas = {'beta' + str(i + 1): np.zeros(dims[i + 1])
+                 for i in range(len(dims) - 2)}
+
+        self.params.update(betas)
+        self.params.update(gammas)
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -312,7 +321,7 @@ class FullyConnectedNet(object):
         if self.use_batchnorm and idx != self.L:
             gamma = self.params['gamma' + str(idx)]
             beta = self.params['beta' + str(idx)]
-            bn_param = self.bn_params['bn_param' + str(idx)]
+            bn_param = self.bn_params[i]
 
         # Computing of the forward pass.
         # Special case of the last layer (output)
@@ -421,3 +430,37 @@ class FullyConnectedNet(object):
     ############################################################################
 
     return loss, grads
+
+def affine_norm_relu_forward(x, w, b, gamma, beta, bn_param):
+    """
+    Convenience layer that perorms an affine transform followed by a ReLU
+
+    Inputs:
+    - x: Input to the affine layer
+    - w, b: Weights for the affine layer
+    - gamma, beta : Weight for the batch norm regularization
+    - bn_params : Contain variable use to batch norml, running_mean and var
+
+    Returns a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to the backward pass
+    """
+
+    h, h_cache = affine_forward(x, w, b)
+    hnorm, hnorm_cache = batchnorm_forward(h, gamma, beta, bn_param)
+    hnormrelu, relu_cache = relu_forward(hnorm)
+    cache = (h_cache, hnorm_cache, relu_cache)
+
+    return hnormrelu, cache
+
+def affine_norm_relu_backward(dout, cache):
+    """
+    Backward pass for the affine-relu convenience layer
+    """
+    h_cache, hnorm_cache, relu_cache = cache
+
+    dhnormrelu = relu_backward(dout, relu_cache)
+    dhnorm, dgamma, dbeta = batchnorm_backward_alt(dhnormrelu, hnorm_cache)
+    dx, dw, db = affine_backward(dhnorm, h_cache)
+
+    return dx, dw, db, dgamma, dbeta
